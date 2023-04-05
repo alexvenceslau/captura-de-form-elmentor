c5c1c49cc6e4f5e6139553e955e7c487fd7cb694
alexvenceslau <alex@agenciabamboo.com.br>
2023-04-05 18:12:49 -0300

    Primeiro envio

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # captura-de-form-elmentor
+
+<h1>Bem-vindo à biblioteca de captura de leads do WordPress com Elementor!</h1>
+
+<p>
+Esta biblioteca JavaScript permite que você capture facilmente leads de formulários criados com o plugin Elementor no seu site WordPress. É especialmente útil se você estiver executando uma campanha de marketing digital e precisar coletar informações de contato de seus visitantes.
+</p>
+
+<p>
+Para utilizar a biblioteca de captura de leads com o Elementor, siga os seguintes passos:
+</p>
+
+<ul>
+<li>Baixe o script "elementor-lead-capture.js".</li>
+
+<li>Adicione o script ao seu site WordPress: Vá até o menu do Elementor, selecione "Código Personalizado" e cole o código em "Adicionar código JavaScript".</li>
+
+<li>Localize a variável "pathWebhook" no script e altere o valor para a URL que você criou na integração. Certifique-se de que a URL esteja correta para que as informações de contato dos leads sejam enviadas corretamente.</li>
+</ul>
+
+<p>
+Pronto! Agora você está pronto para capturar leads em seus formulários Elementor. Se você tiver alguma dúvida ou precisar de ajuda, por favor, não hesite em entrar em contato com nossa equipe de suporte. 
+</p>
+<p>
+Obrigado por utilizar nossa biblioteca!
+</p>
---
+++ script.js
@@ -0,0 +1,62 @@
+<script type="text/javascript">
+$ = jQuery;
+var pathWebhook = 'XXXXXXXXXXXXXXXX';
+$(document).ready(function() {
+    $(":submit").click(function(e){
+      e.preventDefault();
+      $form = $(this).parents('form');
+      var formName = $form.attr('name');
+	
+      erro = '';
+        $($form).find('[required="required"]').each(function(){
+            $campo = $(this);
+            console.log($campo.val());
+            if($campo.val() == ''){
+                erro = 'Verifique os campos acima.';
+                $campo.addClass('erro');
+            }
+        });
+            
+
+  
+      if(erro == ''){
+            var dados = {
+                "nome": $('input[name="form_fields[nome]"]').val(),
+                "email": $('input[name="form_fields[email]"]').val(),
+                "telefone": $('input[name="form_fields[telefone]"]').val()
+            };
+            
+            if($('input[name="form_fields[produto_nome]"]').length > 0){
+                dados['produto_nome'] = $('input[name="form_fields[produto_nome]"]').val();
+            }else{
+                dados['form_name'] = formName;
+            }
+            
+            if($('input[name="form_fields[imovel]"]').length > 0){
+                dados['observacao'] = 'cliente interessado no produto: '+$('input[name="form_fields[imovel]"]').val();
+            }
+
+            $.ajax(
+                {
+                type: "POST",
+                contentType: "application/json",
+                url: 'https://app.imobilead.me/integrate-api/integracoes/webhook/'+pathWebhook,
+                data: JSON.stringify(dados),
+                success: function(txt) {
+                    if(txt.message =='okay') {
+                        console.log('Dados enviados com sucesso para Imobilead!');
+                        $form.submit();
+                    } else{
+                        console.log('Erro ao enviar dados para Imobilead!');
+                        $form.submit();
+                    }
+                    
+                }
+                }
+            );
+
+      }
+  });
+});
+	</script>
+
